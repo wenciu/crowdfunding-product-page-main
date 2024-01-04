95daff85f5faca8c5668124e916728940f4b51c1
wenciu <72387149+wenciu@users.noreply.github.com>
2024-01-04 15:31:33 +0100

    .

--- js/index.js
+++ js/index.js
@@ -18,6 +18,13 @@ const overlay = document.querySelector(".modal-overlay");
 // MODAL CONTAINER
 const modal = document.querySelector(".modal-container");
 
+// HIDE MODAL ON PAGE LOAD
+
+window.onload = function () {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+};
+
 // MODAL CLOSE BTN
 const closeBtn = document.querySelector(".close-btn");
 
@@ -35,6 +42,7 @@ bookBtn.addEventListener("click", () => {
   }
 });
 
+//---------------------------------------//
 // RADIO BUTTONS-HIDDEN DIVS FUNCTIONALITY
 
 // HIDDEN DIVS
@@ -51,7 +59,7 @@ cards.forEach(function (card) {
       // Get the target div ID from the data-target attribute
       const targetDivId = this.getAttribute("data-target");
 
-      // Hide divs with not matching IDs
+      // Hide divs with if IDs do not match
       hiddenDivs.forEach(function (div) {
         div.style.display = "none";
       });
@@ -65,6 +73,7 @@ cards.forEach(function (card) {
     });
   });
 });
+//---------------------------------------//
 
 // SUCCESS MODAL CONTAINER
 const successModal = document.querySelector(".success-modal-container");
@@ -95,3 +104,83 @@ closeBtn.addEventListener("click", () => {
 gotItBtn.addEventListener("click", () => {
   successModal.style.display = "none";
 });
+
+// DONATE PROJECT
+
+const inputField1 = document.querySelector(".input-field-1");
+const inputField2 = document.querySelector(".input-field-2");
+const continueBtn = document.querySelectorAll(".continue");
+const totalSumElement = document.querySelector(".total-value");
+const itemsLeftElement1 = document.querySelector(".left-1");
+const itemsLeftElement11 = document.querySelector(".left-1-1");
+const itemsLeftElement2 = document.querySelector(".left-2");
+const itemsLeftElement22 = document.querySelector(".left-2-2");
+
+// Event listener to add input value to Total Sum
+
+// Initial value - total sum
+let totalSum = 89.914;
+totalSumElement.textContent = totalSum;
+
+// Initial value = left items
+let itemsLeft1 = 101;
+let itemsLeft2 = 64;
+
+continueBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const selectedCounter = btn.getAttribute("data-counter");
+
+    // Get values from the input fields
+    const value1 = parseInt(inputField1.value);
+    const value2 = parseInt(inputField2.value);
+
+    const minValue1 = 25; // minimum value for inputField1
+    const minValue2 = 75; // minimum value for inputField2
+
+    // Check if input values are empty or less than a unique minimum value
+    if (
+      isNaN(value1) ||
+      isNaN(value2) ||
+      value1 < minValue1 ||
+      value2 < minValue2
+    ) {
+      alert(
+        "Please enter valid input values that meet the minimum requirements."
+      );
+      return; // Prevent further execution if values are empty
+    }
+
+    // Add the input values to the existing value
+    totalSum += value1 + value2;
+
+    // Update the existing value display
+    totalSumElement.textContent = totalSum;
+
+    // Clear the input fields once value added
+    inputField1.value = "";
+    inputField1.value = "";
+
+    // Update available items left - Element 1
+
+    if (selectedCounter === "value1") {
+      if (itemsLeft1 > 0) {
+        itemsLeft1 -= 1;
+        itemsLeftElement1.textContent = itemsLeft1;
+        itemsLeftElement11.textContent = itemsLeft1;
+      } else {
+        alert("No items left!");
+      }
+    } else if (selectedCounter === "value2") {
+      // Update available items left - Element 2
+      if (itemsLeft2 > 0) {
+        itemsLeft2 -= 1;
+        itemsLeftElement2.textContent = itemsLeft2;
+        itemsLeftElement22.textContent = itemsLeft2;
+      } else {
+        alert("No items left!");
+      }
+    }
+  });
+});
+
+//---------------------------------------//
